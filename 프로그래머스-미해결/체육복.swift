c28f36960c80eccd96138f7ef5584e08deb6a646
Yujin Lee <yj587878@gmail.com>
2023-01-15 22:10:32 +0900

    [ADD] 체육복 미완성 코드 추가

---
+++ 프로그래머스-미해결/체육복.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ n:Int, _ lost:[Int], _ reserve:[Int]) -> Int {
+    var answer = n - lost.count
+    var reserve = reserve
+    for i in 0 ..< lost.count{
+        for j in 0 ..< reserve.count{
+            if reserve[j] == lost[i]-1{
+                answer = answer + 1
+                reserve.remove(at: j)
+                break
+            }else if reserve[j] == lost[i] + 1{
+                answer = answer + 1
+                reserve.remove(at: j)
+                break
+            }
+        }
+    }
+    return answer
+}
