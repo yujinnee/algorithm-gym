61312de9cff513388ab04be44580f53d876974b0
Yujin Lee <51031771+yujinnee@users.noreply.github.com>
2023-02-05 22:05:20 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.21 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.swift
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.swift
@@ -6,8 +6,9 @@ func solution(_ operations:[String]) -> [Int] {
     for i in operations {
         let command = i.split(separator: " ").map({String($0)})
         
-        if command[0] == "I" { queue.append(Int(command[1])!) }
-        else if command[0] == "D" && !queue.isEmpty {
+        if command[0] == "I" { 
+            queue.append(Int(command[1])!) 
+        }else if command[0] == "D" && !queue.isEmpty {
             if command[1]=="-1" { 
                 queue.sort(by: >)
                 queue.removeLast() 
@@ -16,6 +17,10 @@ func solution(_ operations:[String]) -> [Int] {
                 queue.removeLast() }
         }
     }
+    var answer = [0,0]
+    if !(queue.isEmpty){
+        answer = [queue.max()!, queue.min()!]
+    }
     
-    return queue.isEmpty ? [0, 0] : [queue.max()!, queue.min()!]
+    return answer
 }
