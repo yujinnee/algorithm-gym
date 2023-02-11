ac27b656c1366d8e1b452423043f5a227b8238ca
Yujin Lee <51031771+yujinnee@users.noreply.github.com>
2023-02-11 22:31:19 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12980. 점프와 순간 이동/점프와 순간 이동.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ n:Int) -> Int 
+{   var n = n
+    var ans = 0 
+    while(n>0){
+        if(n%2 == 0){
+            n=n/2
+        }else{
+            n = n-1
+            ans += 1
+        }
+    }
+
+    return ans
+}
