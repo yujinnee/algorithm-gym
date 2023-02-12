286dc02f40294d7b74118f8f49d53788611e2ff1
Yujin Lee <51031771+yujinnee@users.noreply.github.com>
2023-02-12 18:03:25 +0900

    [level 2] Title: 삼각 달팽이, Time: 57.47 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.swift
+++ 프로그래머스/lv2/68645. 삼각 달팽이/삼각 달팽이.swift
@@ -3,25 +3,24 @@ import Foundation
 func solution(_ n:Int) -> [Int] {
     var n = n
     var array = Array(repeating: Array(repeating: 0,count : n),count:n)
-    
-    var k = -1
-    var l = 0
-    var num = 1
-    var count = 0
+    var k = -1//행
+    var l = 0//열
+    var num = 1//하나씩 채워나갈 숫자
+    var count = 0 //세방향중 어느방향인지 확인용
     for i in stride(from:n,to:0,by:-1){
         count += 1
         for j in 0..<i{
-
+            
             switch count%3{
             case 0://대각선
-                    k -= 1
-                    l -= 1
+                k -= 1
+                l -= 1
                 array[k][l] = num
                 num += 1
                 
             case 1://아래로
                 
-                    k += 1
+                k += 1
                 array[k][l] = num
                 num += 1
             case 2://옆으로
@@ -42,3 +41,4 @@ func solution(_ n:Int) -> [Int] {
     
     return answer
 }
+
