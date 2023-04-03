a3c954d86aacaa7ce7964fddd9d0c0932c3c9031
Yujin Lee <51031771+yujinnee@users.noreply.github.com>
2023-04-03 13:03:07 +0900

    [level 2] Title: [3차] n진수 게임, Time: 7.68 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.swift
@@ -1,9 +1,4 @@
 func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
-    //진법
-    //미리구할 숫자 t
-    //게임참가인원 m
-    //튜브 순서 p
-    // 123456789 10 11 12 13 14 15 16
     var answer = ""
     var decimal = 0 
     var nums = [Character]()
@@ -16,8 +11,6 @@ func solution(_ n:Int, _ t:Int, _ m:Int, _ p:Int) -> String {
     for i in 0..<finalOrder where i%m==p-1{
         answer.append(nums[i])
     }
-    
-    // answer.removeLast()
     
     return answer
 }
