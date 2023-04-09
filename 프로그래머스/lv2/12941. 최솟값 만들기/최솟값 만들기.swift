d6ecdcc9726558b9793592b66234a19f94bca1ed
Yujin Lee <51031771+yujinnee@users.noreply.github.com>
2023-04-09 19:30:40 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.04 ms, Memory: 16.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func solution(_ A:[Int], _ B:[Int]) -> Int
+{
+    var ans = 0
+
+    var a = A.sorted(by:>)
+    var b = B.sorted(by:<)
+    for i in 0..<A.count{
+        ans += a[i]*b[i]
+    }
+    return ans
+}
