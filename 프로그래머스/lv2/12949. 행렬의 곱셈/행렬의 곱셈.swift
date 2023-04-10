8eaf22aa1b7ef30cd532b30816e2a0cb0811efc9
Yujin Lee <51031771+yujinnee@users.noreply.github.com>
2023-04-10 20:45:59 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.69 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12949. 행렬의 곱셈/행렬의 곱셈.swift
@@ -0,0 +1,16 @@
+import Foundation
+
+func solution(_ arr1:[[Int]], _ arr2:[[Int]]) -> [[Int]] {
+    
+    var answer = Array(repeating: Array(repeating: 0, count: arr2[0].count), count: arr1.count)
+    
+    for i in 0..<answer.count {
+        for j in 0..<answer[i].count {
+            for k in 0..<arr1[0].count {
+                answer[i][j] += arr1[i][k] * arr2[k][j]
+            }
+        }
+    }
+
+    return answer
+}
