4d9633349f61ed34ca4b74bc499af19296df3a70
Yujin Lee <51031771+yujinnee@users.noreply.github.com>
2023-04-13 23:16:24 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.02 ms, Memory: 16.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.swift
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.swift
@@ -1,3 +1,16 @@
 func solution(_ arr:[Int]) -> Int {
-    return 0
+    return arr.reduce(1) { lcm(a: $0, b: $1)}
+}
+
+func gcd(a: Int, b: Int) -> Int {
+    let r = a%b 
+    if r != 0 {
+        return gcd(a: b, b: r) 
+    } else {
+        return b
+    }
+}
+
+func lcm(a: Int, b: Int) -> Int {
+    return a * b / gcd(a: a, b: b) 
 }
