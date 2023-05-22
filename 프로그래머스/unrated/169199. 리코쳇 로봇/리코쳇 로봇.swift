02d78dcf8560b82da671987a92b8d03e0ea78ad5
Yujin Lee <51031771+yujinnee@users.noreply.github.com>
2023-05-22 18:01:57 +0900

    [unrated] Title: 리코쳇 로봇, Time: 0.62 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.swift
+++ 프로그래머스/unrated/169199. 리코쳇 로봇/리코쳇 로봇.swift
@@ -1,86 +1,46 @@
-func solution(_ board: [String]) -> Int {
-    var visited = Array(repeating: Array(repeating: Int.max, count: board[0].count), count: board.count)
-    var queue = [[Int]]()
-    var goalI = 0
-    var goalJ = 0
 
-    for i in 0 ..< board.count {
-        for j in 0 ..< board[0].count {
-            let row = Array(board[i])
-            if row[j] == "R" {
-                queue.append([i,j])
-                visited[i][j] = 0
-            } else if row[j] == "G" {
-                goalI = i
-                goalJ = j
+import Foundation
+
+func solution(_ board:[String]) -> Int {
+    var board = board.map { $0.map { String($0) } }
+    let n = board.count, m = board[0].count
+    var visit = Array(repeating: Array(repeating: Array(repeating: false, count: 4), count: m), count: n)
+    let dx = [0, 0, 1, -1], dy = [1, -1, 0, 0]
+
+    var queue = [(Int, Int, Int)]()
+    var front = 0
+
+    for i in 0..<board.count {
+        let j = board[i].enumerated().filter { $0.element == "R" }.map { $0.offset }
+        if !j.isEmpty {
+            queue.append((i, j[0], 0))
+            board[i][j[0]] = "."
+            break
+        }
+    }
+
+    func move(_ robot: (Int, Int), _ i: Int) -> (Int, Int) {
+        var (x, y) = (robot.0, robot.1)
+        while true {
+            let (nX, nY) = (x + dx[i], y + dy[i])
+            if nX < 0 || nY < 0 || nX >= n || nY >= m || board[nX][nY] == "D" { break }
+            (x, y) = (nX, nY)
+        }
+        return (x, y)
+    }
+
+    while front < queue.count {
+        let (x, y, count) = queue[front]
+        front += 1
+        for i in 0...3 {
+            let (nX, nY) = move((x, y), i)
+            if board[nX][nY] == "G" { return count + 1 }
+            if visit[nX][nY][i] == false {
+                visit[nX][nY][i] = true
+                queue.append((nX, nY, count + 1))
             }
         }
     }
 
-    repeat {
-        let item = queue.removeFirst()
-
-        // 아래로 Y값을 증가시키며 가봅시다.
-        var curI = item[0]
-        var curJ = item[1]
-        repeat {
-            let row = Array(board[curI])
-            if row[curJ] == "D" {
-                break
-            }
-            curI += 1
-        } while (curI < board.count)
-        if visited[curI-1][curJ] > visited[item[0]][item[1]] + 1 {
-            visited[curI-1][curJ] = visited[item[0]][item[1]] + 1
-            queue.append([curI-1, curJ])
-        }
-
-        // 위로
-        curI = item[0]
-        curJ = item[1]
-        repeat {
-            let row = Array(board[curI])
-            if row[curJ] == "D" {
-                break
-            }
-            curI -= 1
-        } while (curI >= 0)
-        if visited[curI+1][curJ] > visited[item[0]][item[1]] + 1 {
-            visited[curI+1][curJ] = visited[item[0]][item[1]] + 1
-            queue.append([curI+1, curJ])
-        }
-
-        // 오른쪽으로
-        curI = item[0]
-        curJ = item[1]
-        var row = Array(board[curI])
-        repeat {
-            if row[curJ] == "D" {
-                break
-            }
-            curJ += 1
-        } while (curJ < board[0].count)
-        if visited[curI][curJ-1] > visited[item[0]][item[1]] + 1 {
-            visited[curI][curJ-1] = visited[item[0]][item[1]] + 1
-            queue.append([curI, curJ-1])
-        }
-
-        // 왼쪽으로
-        curI = item[0]
-        curJ = item[1]
-        row = Array(board[curI])
-        repeat {
-            if row[curJ] == "D" {
-                break
-            }
-            curJ -= 1
-        } while (curJ >= 0)
-        if visited[curI][curJ+1] > visited[item[0]][item[1]] + 1 {
-            visited[curI][curJ+1] = visited[item[0]][item[1]] + 1
-            queue.append([curI, curJ+1])
-        }
-
-    } while (!queue.isEmpty)
-
-    return visited[goalI][goalJ] == Int.max ? -1 : visited[goalI][goalJ]
+    return -1
 }
