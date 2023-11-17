7cbe5dbe3a16f7915f8480b66cbbe46b2eb30643
Yujin Lee (Lizzy) <51031771+yujinnee@users.noreply.github.com>
2023-11-17 13:44:33 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 8 ms, Memory: 69100 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.swift
@@ -0,0 +1,16 @@
+let n = Int(readLine()!)!
+var dp = [Int](repeating: 0, count: n+1)
+
+if n == 1 {
+    print(1)
+} else if n == 2 {
+    print(3)
+} else {
+    dp[1] = 1
+    dp[2] = 3
+    
+    for i in 3..<n+1 {
+        dp[i] = (dp[i-1] + 2*dp[i-2]) % 10007
+    }
+    print(dp[n])
+}
