8813f6ebbaf849ddbd45dbfd0f89e8d260f94afc
Yujin Lee (Lizzy) <51031771+yujinnee@users.noreply.github.com>
2023-11-17 13:50:05 +0900

    [Gold IV] Title: 드래곤 앤 던전, Time: 212 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.swift
+++ 백준/Gold/16434. 드래곤 앤 던전/드래곤 앤 던전.swift
@@ -5,7 +5,6 @@ let n:Int = input[0]
 var myAtk:Int = input[1]
 
 var needHp:Int = 0 // 필요한 hp 양들을 쌓아나간다고 생각
-var havePotion:Int = 0
 var ans:Int = 0
 for _ in 0..<n{
     input = readLine()!.split(separator: " ").compactMap{Int($0)!}
@@ -22,16 +21,10 @@ for _ in 0..<n{
         //이번 라운드에서 필요한 최소 체력 계산
         needHp += (enemyAtk*hittedCount)//딜 맞는 양
         
-         // 딜 맞는 수만큼 버틸 체력 필요함.최소 체력 1남아있어야 되서 1더하기
-
-//        if(enemyHp%myAtk == 0){needHp += enemyAtk*((enemyHp/myAtk) - 1)}
-//        else {needHp += enemyAtk*(enemyHp/myAtk) }
-        
     }else {// 포션 일 때
         let potionPower:Int = input[1]
         let potionLife:Int = input[2]
         myAtk += potionPower
-//        havePotion += potionLife
         needHp -= potionLife
         if(needHp<0) {needHp = 0 }
         
