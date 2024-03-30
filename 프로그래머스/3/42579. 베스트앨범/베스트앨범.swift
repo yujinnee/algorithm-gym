52840b67260d9e934a0fc21712b72ecd393ebd8a
Yujin Lee (Lizzy) <51031771+yujinnee@users.noreply.github.com>
2024-03-30 19:04:01 +0900

    [level 3] Title: 베스트앨범, Time: 0.26 ms, Memory: 16.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.swift
@@ -0,0 +1,45 @@
+import Foundation
+
+func solution(_ genres:[String], _ plays:[Int]) -> [Int] {
+    var allMusic = Dictionary<String,Array<(Int,Int)>>()
+    var genreCounter = Dictionary<String,Int>()
+    for i in 0..<genres.count {
+        let genreKey = genres[i]
+        if allMusic[genreKey] == nil {
+             allMusic[genreKey] = [(Int,Int)]()
+            genreCounter[genreKey] = 0
+        }
+       
+        allMusic[genreKey]!.append((i,plays[i]))
+        genreCounter[genreKey]! += plays[i]
+        // print(i,plays[i])
+
+    }
+    // print(allMusic)
+    // print("###")
+    // print(genreCounter)
+
+    let sortedArray = genreCounter.sorted{$0.value>$1.value}.map{($0.key,$0.value)}
+    // print(sortedArray[0])
+    //  print("###")
+    
+    var answer = [Int]()
+    for item in sortedArray {
+        let genreKey = item.0
+        let playSorted = allMusic[genreKey]!.sorted{$0.1>$1.1 || $0.0<$1.0}.map{$0.0}
+         // print(">>>")
+        // print(playSorted)
+       
+        if(playSorted.count >= 2) {
+            answer.append(playSorted[0])
+            answer.append(playSorted[1])
+        } else {
+            answer.append(playSorted[0])
+        }
+       
+        // print(answer)
+        // print(">>>")
+    }
+    // print(allMusic)
+    return answer
+}
