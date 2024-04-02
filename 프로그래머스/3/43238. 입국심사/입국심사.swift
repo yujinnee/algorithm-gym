6b7378681a605a78c6bad002bcb047a724253766
Yujin Lee (Lizzy) <51031771+yujinnee@users.noreply.github.com>
2024-04-02 13:25:25 +0900

    [level 3] Title: 입국심사, Time: 0.16 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.swift
+++ 프로그래머스/3/43238. 입국심사/입국심사.swift
@@ -1,37 +1,60 @@
+
 import Foundation
 
 func solution(_ n:Int, _ times:[Int]) -> Int64 {
-    var judges = times
-    // judges.sorted()
-    let minTime = judges[0]
-    func getPersonNum(_ time: Int) -> Int { // 이 시간안에 최대 인원
-        let sum = judges.map{time/$0}.reduce(0,+)
-        // print(sum)
-        return sum
-    }
-    
-    
-    // print(judges)
-    
-    var start = 1
-    var end = minTime*n
-    var count = 0
-    while true {
-        count += 1
-        // print("start\(start)")
-        // print("end\(end)")
-        var mid = (start + end) % 2 == 0 ? (start + end) / 2 : (start + end) / 2 + 1
-        // print("mid\(mid)")
-        let personNum = getPersonNum(mid)
-        // print("personNum\(personNum)")
-        if personNum >= n {
-            end = mid
-        }else {
-            start = mid
+
+    guard let maxInTimes = times.max() else { return 0 }
+
+    var left: Int64 = 0
+    var right: Int64 = Int64(maxInTimes * n)
+    var mid: Int64 = (left + right) / 2
+    var answer: Int64 = right
+
+    while left <= right {
+        mid = (left + right) / 2
+        var possible: Int64 = 0
+        for time in times {
+            possible = possible + Int64((Int(mid) / time))
         }
-        if start == end || start + 1 == end { break }
+        // n보다 작을 경우 해당 mid 값 보단 더 커야한다.
+        if possible >= n {
+            answer = mid
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
     }
-    // print(start)
-    
-    return Int64(end)
+
+    return answer
 }
+
+// import Foundation
+
+// func solution(_ n:Int, _ times:[Int]) -> Int64 {
+//     var judges = times
+
+//     let minTime = judges[0]
+//     func getPersonNum(_ time: Int) -> Int { // 이 시간안에 최대 인원
+//         let sum = judges.map{time/$0}.reduce(0,+)
+//         return sum
+//     }
+    
+//     var start = 1
+//     var end = minTime*n
+//     var count = 0
+//     while true {
+//         count += 1
+//         var mid = (start + end) % 2 == 0 ? (start + end) / 2 : (start + end) / 2 + 1
+
+//         let personNum = getPersonNum(mid)
+
+//         if personNum >= n {
+//             end = mid
+//         }else {
+//             start = mid
+//         }
+//         if start == end || start + 1 == end { break }
+//     }
+    
+//     return Int64(end)
+// }
